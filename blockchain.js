dd435d60fe506b52da4c95dff2bc12292c652bf4
dan4oolegovdan4ev-star <dan4oolegovdan4ev@gmail.com>
2026-02-08 00:26:41 +0200

    Update blockchain.js

--- blockchain.js
+++ blockchain.js
@@ -1,27 +1,58 @@
-let blockchain = [];
-let mempool = [];
+import { Block } from "./block.js";
+import { Transaction } from "./tx.js";
 
-function genesisBlock() {
-  return {
-    index: 0,
-    previousHash: "0",
-    nonce: 0,
-    transactions: [],
-    hash: "GENESIS"
-  };
-}
-
-if (blockchain.length === 0) {
-  blockchain.push(genesisBlock());
-}
-
-function calculateBalance(addr) {
-  let bal = 0;
-  for (const b of blockchain) {
-    for (const tx of b.transactions) {
-      if (tx.to === addr) bal += tx.amount;
-      if (tx.from === addr) bal -= tx.amount;
-    }
+export class Blockchain {
+  constructor() {
+    this.chain = [new Block(Date.now(), [], "0")];
+    this.pending = [];
+    this.diff = 4;
+    this.reward = 25;
+    this.maxSupply = 100_000_000;
+    this.totalSupply = 0;
+    this.halvingInterval = 5000;
+    this.targetTime = 20_000;
   }
-  return bal;
+
+  latest() { return this.chain.at(-1); }
+
+  getBalance(addr) {
+    let bal = 0;
+    for (const b of this.chain)
+      for (const tx of b.txs) {
+        if (tx.from === addr) bal -= tx.amount;
+        if (tx.to === addr) bal += tx.amount;
+      }
+    return bal;
+  }
+
+  currentReward() {
+    return this.reward / (2 ** Math.floor(this.chain.length / this.halvingInterval));
+  }
+
+  adjustDifficulty() {
+    if (this.chain.length < 2) return;
+    const dt = this.latest().ts - this.chain.at(-2).ts;
+    if (dt < this.targetTime) this.diff++;
+    else if (dt > this.targetTime * 2 && this.diff > 1) this.diff--;
+  }
+
+  mineBlock(block) {
+    while (!block.hash.startsWith("0".repeat(this.diff))) {
+      block.nonce++;
+      block.hash = block.calc();
+    }
+    this.chain.push(block);
+    this.adjustDifficulty();
+  }
+
+  mine(miner) {
+    if (this.totalSupply < this.maxSupply) {
+      const r = Math.min(this.currentReward(), this.maxSupply - this.totalSupply);
+      this.pending.push(new Transaction(null, miner, r));
+      this.totalSupply += r;
+    }
+    const b = new Block(Date.now(), this.pending, this.latest().hash);
+    this.mineBlock(b);
+    this.pending = [];
+  }
 }

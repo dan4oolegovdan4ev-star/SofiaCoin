bb9c4ae9db0828405afc2f314664dbbba603a71e
dan4oolegovdan4ev-star <dan4oolegovdan4ev@gmail.com>
2026-02-07 23:23:55 +0200

    Update blockchain.js

--- blockchain.js
+++ blockchain.js
@@ -1,18 +1,28 @@
-let blockchain=[];
-let mempool=[];
+window.blockchain = JSON.parse(localStorage.getItem("sofiaBlockchain") || "[]");
+window.mempool = [];
+window.minedSoFar = Number(localStorage.getItem("sofiaMinedSoFar")) || 0;
 
-function genesis(){
-  return {index:0,transactions:[],previousHash:"0",nonce:0,hash:"0"};
+function genesis() {
+  return {
+    index: 0,
+    transactions: [],
+    previousHash: "0",
+    nonce: 0,
+    hash: "GENESIS"
+  };
 }
 
-blockchain.push(genesis());
+if (blockchain.length === 0) {
+  blockchain.push(genesis());
+  localStorage.setItem("sofiaBlockchain", JSON.stringify(blockchain));
+}
 
-function calculateBalance(addr){
-  let bal=0;
-  for(const b of blockchain){
-    for(const tx of b.transactions){
-      if(tx.to===addr) bal+=tx.amount;
-      if(tx.from===addr) bal-=tx.amount;
+function calculateBalance(addr) {
+  let bal = 0;
+  for (const b of blockchain) {
+    for (const tx of b.transactions) {
+      if (tx.to === addr) bal += tx.amount;
+      if (tx.from === addr) bal -= tx.amount;
     }
   }
   return bal;

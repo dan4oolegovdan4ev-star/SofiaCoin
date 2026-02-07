569c9221aacd9300892b5326115738f500827da4
dan4oolegovdan4ev-star <dan4oolegovdan4ev@gmail.com>
2026-02-07 22:35:50 +0200

    Create blockchain.js

---
+++ blockchain.js
@@ -0,0 +1,19 @@
+let blockchain = JSON.parse(localStorage.getItem("sofiaBlockchain") || "[]");
+let mempool = [];
+
+function genesis(){
+  return {index:0,transactions:[],previousHash:"0",nonce:0,hash:"0"};
+}
+
+if(blockchain.length===0) blockchain.push(genesis());
+
+function calculateBalance(addr){
+  let bal=0;
+  for(const b of blockchain){
+    for(const tx of b.transactions){
+      if(tx.to===addr) bal+=tx.amount;
+      if(tx.from===addr) bal-=tx.amount;
+    }
+  }
+  return bal;
+}
